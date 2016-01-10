35dcac3aafa07b3f1e9566f6c83bbcad075649bb
Nicolas Peri <nperi89@gmail.com>
2016-01-09 23:13:16 -0200

    test

---
+++ src/shaderTest.cpp
@@ -0,0 +1,9 @@
+//
+//  shaderTest.cpp
+//  example
+//
+//  Created by Nicolas Peri on 12/30/15.
+//
+//
+
+#include "shaderTest.h"
---
+++ src/shaderTest.h
@@ -0,0 +1,14 @@
+//
+//  shaderTest.h
+//  example
+//
+//  Created by Nicolas Peri on 12/30/15.
+//
+//
+
+#ifndef __example__shaderTest__
+#define __example__shaderTest__
+
+#include <stdio.h>
+
+#endif /* defined(__example__shaderTest__) */
